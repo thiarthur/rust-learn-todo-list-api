e7cb63f7e36de058cb45e27572f3916f2a2ef43f
Thiago Goslar <thiarthur1@gmail.com>
2024-04-28 16:13:18 -0300

    [FEAT] Implements logger middleware

--- src/main.rs
+++ src/main.rs
@@ -5,18 +5,28 @@ mod model;
 mod response;
 mod routes;
 mod schema;
+
 use dotenvy::dotenv;
+use ntex::web::middleware::Logger;
 use ntex::web::{App, HttpServer};
 use std::io::Result;
 
+use env_logger::Env;
+
 #[ntex::main]
 async fn main() -> Result<()> {
+    env_logger::init_from_env(Env::default().default_filter_or("info"));
     dotenv().ok();
 
     let pool = db::create_pool();
 
-    HttpServer::new(move || App::new().state(pool.clone()).configure(routes::configure))
-        .bind(("127.0.0.1", 8080))?
-        .run()
-        .await
+    HttpServer::new(move || {
+        App::new()
+            .state(pool.clone())
+            .wrap(Logger::default())
+            .configure(routes::configure)
+    })
+    .bind(("127.0.0.1", 8080))?
+    .run()
+    .await
 }
